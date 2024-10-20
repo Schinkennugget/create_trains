07cb2db88edd1d554d16b532715c7523b0e9f906
Schinkennugget <jabo.dichtl@icloud.com>
2024-10-20 13:15:36 +0200

    0.1.0 dev1

---
+++ src/main/java/com/schinkennugget/CreateTrains.java
@@ -0,0 +1,16 @@
+package com.schinkennugget;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CreateTrains implements ModInitializer {
+	public static final String MOD_ID = "create_trains";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
